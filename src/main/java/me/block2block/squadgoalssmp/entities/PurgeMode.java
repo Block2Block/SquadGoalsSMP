4ddd25dff22f18034437cdfc56c0a556a344117c
thebarkerkids@hotmail.com <Ilov3popmusic>
2020-01-05 00:29:37 +0000

    Fixed slight bug with leaving and added more purges

--- src/main/java/me/block2block/squadgoalssmp/entities/PurgeMode.java
+++ src/main/java/me/block2block/squadgoalssmp/entities/PurgeMode.java
@@ -15,7 +15,12 @@ public enum PurgeMode {
     NORMAL(1, "Normal", null, null, null, null, null, null, null, null, "This is a normal purge. All weapons, armor, enchantments and potions are enabled!", true),
     BLOODMOON(2, "Blood Moon", null, null, null, null, null, null, null, null, "This is a Blood Moon purge. All hostile mob spawning has been turned up, spawned mobs are stronger, and it will remain night-time until the purge ends!", true),
     ZOMBIEAPOCALYPSE(3, "Zombie Apocalypse", null, null, null, null, null, null, null, null, "This is a Zombie Apocalypse purge. Uh oh! Walkers have been released into the wild! Zombie spawn-rates have been turned up, and zombies are stronger! It will remain night-time until the purge ends!", true),
-    BASIC(4, "Basic", null, new ArrayList<>(Arrays.asList(WOODEN_SWORD, BOW)), null, new ArrayList<>(Arrays.asList(LEATHER_BOOTS, LEATHER_CHESTPLATE, LEATHER_LEGGINGS, LEATHER_HELMET)), new ArrayList<>(), null, new ArrayList<>(), null, "This is a basic purge, all armor (except leather), weapons (except wooden swords & bows), enchantments, potion effects and Gapples have been disabled!", false);
+    BASIC(4, "Basic", null, new ArrayList<>(Arrays.asList(WOODEN_SWORD, BOW)), null, new ArrayList<>(Arrays.asList(LEATHER_BOOTS, LEATHER_CHESTPLATE, LEATHER_LEGGINGS, LEATHER_HELMET)), new ArrayList<>(), null, new ArrayList<>(), null, "This is a basic purge, all armor (except leather), weapons (except wooden swords & bows), enchantments, potion effects and Gapples have been disabled!", false),
+    NOENCHANT(5, "No Enchant", null, null, null, null, null, null, new ArrayList<>(), null, "This is a No Enchant purge. All enchantments have been disabled!", true),
+    NOPOTION(6, "No Potions", null, null, null, null, new ArrayList<>(), null, null, null, "This is a No Potion purge. All potions and potion effects have been disabled!", true),
+    DIAMONDONLY(7, "All The Diamonds", null, new ArrayList<>(Arrays.asList(DIAMOND_SWORD, BOW)), null, new ArrayList<>(Arrays.asList(DIAMOND_CHESTPLATE, DIAMOND_HELMET, DIAMOND_LEGGINGS, DIAMOND_BOOTS)), null, null, null, null, "This is an All The Diamonds purge. You can only use Diamond Armor and a Diamond Sword! All potions and Enchantments are still enabled!", true),
+    NOARMOR(8, "No Armor", null, null, null, new ArrayList<>(), null, null, null, null, "This is an No Armor purge. All armor is disabled! You must fight unprotected!", true),
+    FISTFIGHT(9, "Fist Fight", null, new ArrayList<>(), null, new ArrayList<>(), null, null, null, null, "This is a Fist Fight purge! All weapons and armor are disabled!", true);
 
     private int id;
     private String name;
@@ -49,6 +54,15 @@ public enum PurgeMode {
     public static PurgeMode getByID(int id) {
         for (PurgeMode p : PurgeMode.values()) {
             if (p.id == id) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public static PurgeMode getByName(String name) {
+        for (PurgeMode p : PurgeMode.values()) {
+            if (p.name().toLowerCase().equals(name.toLowerCase())) {
                 return p;
             }
         }
